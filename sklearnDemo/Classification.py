9ad60f6feaab3a10a69295a75f79a40d0b73813c
191078834 <191078834@qq.com>
2019-05-13 17:52:46 +0800

    添加机械学习分类py文件 还未实现

--- sklearnDemo/Classification.py
+++ sklearnDemo/Classification.py
@@ -3,4 +3,9 @@
 #Auther: WQM
 #Time: 2019/5/13 15:36
 from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
 mnist = input_data.read_data_sets('MNIST', one_hot='True')
+
+
+xs = tf.placeholder(tf.float32, [])
+ys = tf.placeholder(tf.float32, [])
